044b9492620ed196f5ac8c350d26b636e41a7975
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-02-21 17:54:28 +0000

    Login-Logout functionality. Needs to be refined

--- mysite/sis/urls.py
+++ mysite/sis/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from . import views
 
+
+
+app_name = 'sis'
 urlpatterns = [
     path('home/', views.home),
-    path('register/', views.UserFormView.as_view(), name='register'),
+    #path('register/', views.UserFormView.as_view(), name='register'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
+    path('index/', views.index, name="index")
 ]
--- mysite/sis/views.py
+++ mysite/sis/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+
 from django.http import HttpResponseRedirect
 
 from sis.forms import UserForm
@@ -10,41 +12,67 @@ from django.template import RequestContext
 
 from django.views.generic import View
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+
 def home(request):
     output = "LOL"
     return HttpResponse(output)
 
-# class based views
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'sis/registration_form.html'
-    print("Entered here")
-    #display blank form
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form':form})
+def index(request):
+    return HttpResponse("Index view")
 
-    # process form data
-    def post(self, request):
-        form = self.form_class(request.POST)
+def login_user(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)    
+                return render(request, 'sis/home.html')
+            else:
+                return render(request, {'error_message':'Invalid login'})
+    return render(request, 'sis/login.html')
 
-        if form.is_valid():
-            # create an object from the form
-            # without saving it to the database
-            user = form.save(commit=False)
-            # cleaned (normalized) data
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'sis/login.html', context)
 
-            user.set_password(password)
-            # save to the database
-            user.save()
-
-            user = authenticate(email=email, password=password)
-            if user is not None:
-                if user.is_active:
-                    # now logged in
-                    login(request, user)
-                    # request.user.
-                    return redirect('sis:index')
-        return render(request, self.template_name, {'form':form})
+# class UserFormView(View):
+#     form_class = UserForm
+#     template_name = 'sis/registration_form.html'
+#     #display blank form
+#     def get(self, request):
+#         form = self.form_class(None)
+#         return render(request, self.template_name, {'form':form})
+#
+#     # process form data
+#     @csrf_protect
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#
+#         if form.is_valid():
+#             # create an object from the form
+#             # without saving it to the database
+#             user = form.save(commit=False)
+#             # cleaned (normalized) data
+#             email = form.cleaned_data['email']
+#             password = form.cleaned_data['password']
+#
+#             user.set_password(password)
+#             # save to the database
+#             user.save()
+#
+#             user = authenticate(email=email, password=password)
+#             if user is not None:
+#                 if user.is_active:
+#                     # now logged in
+#                     login(request, user)
+#                     # request.user.
+#                     return redirect('sis:index')
+#         return render(request, self.template_name, {'form':form})
