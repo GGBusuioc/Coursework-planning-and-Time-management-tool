11de6d45b34e66fa8b86db5943d594426d522f9c
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-04-29 14:32:23 +0100

    Welcoming message

--- mysite/sis/views.py
+++ mysite/sis/views.py
@@ -307,7 +307,7 @@ def coursework_details(request, module_id, coursework_id):
 
 
         messages.info(request, "You have marked %s as being %d percent complete. " % (coursework.title, form.cleaned_data.get('percentage')))
-        
+
 
         print(form.cleaned_data.get('percentage'))
         object = UserCourseworkMembership.objects.get(user=request.session['user_id'],coursework=coursework_id)
